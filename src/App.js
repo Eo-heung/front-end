2923fe73e34c0f81b77b1dea25037b4cb1c0196e
zener9233 <cite9233@naver.com>
2023-08-01 12:12:37 +0900

    feat-26: password 찾기 화면 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
+import Password3 from './components/Password3';
+
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Password3></Password3>}></Route>
       <Route path='/login' element={<Login></Login>}></Route>
     </Routes>
   );
