b3e59a789217ebbf717c4e01c12abb5a5cbc174a
dxnx-hhh <dxnxhb@gmail.com>
2023-08-02 20:41:22 +0900

    feat-43: mainUI 푸터 구현

---
+++ src/components/partials/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Footer = () => {
+    return (
+        <>
+            <div class="d-sm-flex justify-content-center justify-content-sm-between">
+                <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © bootstrapdash.com 2020</span>
+            </div>
+        </>
+    )
+}
+
+export default Footer
