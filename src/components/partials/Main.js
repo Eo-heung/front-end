352e5b9f9277379ccc196b37b522ed0f23320be0
dxnx-hhh <dxnxhb@gmail.com>
2023-08-02 20:40:58 +0900

    feat-42: mainUI 본문, 사이드바 구현

---
+++ src/components/partials/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Main = () => {
+    return (
+        <>
+            <div class="content-wrapper"></div>
+        </>
+    )
+}
+
+export default Main
---
+++ src/components/partials/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <>
+            <nav class="sidebar sidebar-offcanvas" id="sidebar">
+                <div class="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top">로고자리</div>
+                <div class="profile-desc">프로필자리</div>
+                <div class="작명해주세용">친구화면</div>
+
+            </nav>
+        </>
+    )
+}
+
+export default Nav
