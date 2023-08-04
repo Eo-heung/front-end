dfa12682ed9296ca996ea694027dcfafe5766e37
zener9233 <cite9233@naver.com>
2023-08-04 11:07:20 +0900

    feat-23: join 화면 구현

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router';
+import Join from './components/Join';
+import JoinBirth from './components/JoinBirth';
+import JoinLocal1 from './components/JoinLocal1';
+import JoinLocal2 from './components/JoinLocal2';
+import JoinLocal3 from './components/JoinLocal3';
+import JoinName from './components/JoinName';
+import JoinPhoneNum1 from './components/JoinPhoneNum1';
+import JoinPhoneNum2 from './components/JoinPhoneNum2';
+import JoinPW from './components/JoinPW';
+import JoinComplete from './components/JoinComplete';
+import JoinFavorite from './components/JoinFavorite';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/join' element={<Join></Join>}></Route>
+      <Route path='/join1' element={<JoinPhoneNum1></JoinPhoneNum1>}></Route>
+      <Route path='/join2' element={<JoinPhoneNum2></JoinPhoneNum2>}></Route>
+      <Route path='/join3' element={<JoinPW></JoinPW>}></Route>
+      <Route path='/join4' element={<JoinName></JoinName>}></Route>
+      <Route path='/join5' element={<JoinBirth></JoinBirth>}></Route>
+      <Route path='/join6' element={<JoinLocal1></JoinLocal1>}></Route>
+      <Route path='/join7' element={<JoinLocal2></JoinLocal2>}></Route>
+      <Route path='/join8' element={<JoinLocal3></JoinLocal3>}></Route>
+      <Route path='/join9' element={<JoinFavorite></JoinFavorite>}></Route>
+      <Route path='/join10' element={<JoinComplete></JoinComplete>}></Route>
+    </Routes>
   );
 }
 
---
+++ src/components/JoinPhoneNum2.js
@@ -0,0 +1,153 @@
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import LinearProgress from '@mui/material/LinearProgress';
+import Link from '@mui/material/Link';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import React, { useEffect, useState } from 'react';
+import { styled } from '@mui/system';
+import thumbImage from '../public/image.png.png';
+
+const JoinPhoneNum2 = () => {
+    const [progress, setProgress] = useState(0);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
+    };
+
+    // 원의 left 값을 progress에 바인딩하기 위해 styled 컴포넌트 대신 일반 함수 컴포넌트를 사용합니다.
+    const Circle = styled('div')(({ progress }) => ({
+        position: 'absolute',
+        left: `calc(${progress}% - 5px)`,
+        top: '50%',
+        transform: 'translateY(-50%)',
+        width: '40px',
+        height: '40px',
+        backgroundImage: `url(${thumbImage})`, // 가져온 이미지를 배경 이미지로 사용합니다.
+        backgroundSize: 'cover', // 필요에 따라 배경 이미지 크기를 조절합니다.
+        zIndex: 2,
+        transition: "left 500ms ease-out"
+    }));
+
+    const defaultTheme = createTheme();
+
+    function LinearProgressWithLabel() {
+        const [progress, setProgress] = useState(14.2857);
+
+        useEffect(() => {
+            const timer = setTimeout(() => {
+                setProgress(28.5714);
+            }, 500);
+
+            return () => {
+                clearTimeout(timer);
+            };
+        }, []);
+
+
+        return (
+            <Box sx={{ position: 'relative', display: 'flex', alignItems: 'center', width: '100%', height: '20px' }}>
+                <Box sx={{ position: 'relative', flex: 14 }}>
+                    <LinearProgress variant="determinate" value={progress} />
+                    <Circle progress={progress} />
+                </Box>
+                <Box sx={{ flex: 1, marginLeft: 1 }}>
+                    <Typography variant="body2" color="text.secondary">{`${Math.round(progress)}%`}</Typography>
+                </Box>
+            </Box>
+        );
+    }
+
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: '#E55C25',
+            },
+        },
+    });
+
+
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        height: '608.57px',
+                        marginTop: 8
+                    }}
+                >
+                    <Typography variant="h5" fontSize="10pt" gutterBottom textAlign={'center'}>
+                        어흥이 인증번호를 보내드렸어요!
+                    </Typography>
+                    <br></br>
+                    <br></br>
+                    <Typography variant="h6" fontSize="20pt" textAlign={'center'}>
+                        문자함에서 확인한
+                        <br></br>
+                        내 인증번호는?
+                    </Typography>
+                    <br></br>
+                    <br></br>
+                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3, width: '100%' }}>
+                        <Grid container spacing={2} >
+                            <Grid item xs={12} >
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="code"
+                                    label="인증번호 입력"
+                                    name="code"
+                                    autoComplete="off"
+                                />
+                            </Grid>
+                        </Grid>
+
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                                mt: 3,
+                            }}
+                        >
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                sx={{
+                                    mt: 3,
+                                    mb: 2,
+                                    backgroundColor: '#FFB471', // 평소 색상
+                                    '&:hover': {
+                                        backgroundColor: '#E55C25', // 호버 시 색상
+                                    },
+                                }}
+                            >
+                                인증하기
+                            </Button>
+
+                            <ThemeProvider theme={theme}>
+                                <Box sx={{ width: '100%', marginTop: "47%" }}>
+                                    <LinearProgressWithLabel value={progress} />
+                                </Box>
+                            </ThemeProvider>
+                        </Box>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
+    );
+};
+
+export default JoinPhoneNum2;
