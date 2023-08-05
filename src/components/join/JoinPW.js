97f235dce106045ad877e490a94aea8dc58b5215
Jay3404 <111181171+Jay3404@users.noreply.github.com>
2023-08-05 10:50:39 +0900

    feat-23: 회원가입 페이지 하나의 URL에서 해결할 수 있도록 수정.

---
+++ src/components/join/JoinPW.js
@@ -0,0 +1,159 @@
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import LinearProgress from '@mui/material/LinearProgress';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { styled } from '@mui/system';
+import React, { useEffect, useState } from 'react';
+import thumbImage from '../../public/image.png.png';
+
+const JoinPW = () => {
+    const [progress, setProgress] = useState(0);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
+    };
+
+    const Circle = styled('div')(({ progress }) => ({
+        position: 'absolute',
+        left: `calc(${progress}% - 5px)`,
+        top: '50%',
+        transform: 'translateY(-50%)',
+        width: '40px',
+        height: '40px',
+        backgroundImage: `url(${thumbImage})`, // 가져온 이미지를 배경 이미지로 사용합니다.
+        backgroundSize: 'cover', // 필요에 따라 배경 이미지 크기를 조절합니다.
+        zIndex: 2,
+        transition: "left 500ms ease-out"
+    }));
+
+    const defaultTheme = createTheme();
+
+    function LinearProgressWithLabel() {
+        const [progress, setProgress] = useState(28.5714);
+
+        useEffect(() => {
+            const timer = setTimeout(() => {
+                setProgress(42.8571);
+            }, 500);
+
+            return () => {
+                clearTimeout(timer);
+            };
+        }, []);
+
+
+        return (
+            <Box sx={{ position: 'relative', display: 'flex', alignItems: 'center', width: '100%', height: '20px' }}>
+                <Box sx={{ position: 'relative', flex: 14 }}>
+                    <LinearProgress variant="determinate" value={progress} />
+                    <Circle progress={progress} />
+                </Box>
+                <Box sx={{ flex: 1, marginLeft: 1 }}>
+                    <Typography variant="body2" color="text.secondary">{`${Math.round(progress)}%`}</Typography>
+                </Box>
+            </Box>
+        );
+    }
+
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: '#E55C25',
+            },
+        },
+    });
+
+
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        height: '608.57px',
+                        marginTop: 8
+                    }}
+                >
+                    <Typography variant="h5" fontSize="10pt" gutterBottom textAlign={'center'}>
+                        어흥 입장시에 사용할
+                    </Typography>
+                    <br></br>
+                    <br></br>
+                    <Typography variant="h6" fontSize="20pt" textAlign={'center'}>
+                        비밀번호를
+                        <br></br>
+                        입력해주세요
+                    </Typography>
+                    <br></br>
+                    <br></br>
+                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3, width: '100%' }}>
+                        <Grid container spacing={2} >
+                            <Grid item xs={12} >
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="code"
+                                    label="비밀번호 입력"
+                                    name="code"
+                                    autoComplete="off"
+                                />
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="code"
+                                    label="한번 더 입력해주세요"
+                                    name="code"
+                                    autoComplete="off"
+                                />
+                            </Grid>
+                        </Grid>
+
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                                mt: 3,
+                            }}
+                        >
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                sx={{
+                                    mt: 3,
+                                    mb: 2,
+                                    backgroundColor: '#FFB471', // 평소 색상
+                                    '&:hover': {
+                                        backgroundColor: '#E55C25', // 호버 시 색상
+                                    },
+                                }}
+                            >
+                                입력완료
+                            </Button>
+
+                            <ThemeProvider theme={theme}>
+                                <Box sx={{ width: '100%', marginTop: "47%" }}>
+                                    <LinearProgressWithLabel value={progress} />
+                                </Box>
+                            </ThemeProvider>
+                        </Box>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
+    );
+};
+
+export default JoinPW;
