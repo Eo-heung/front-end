02cb5e7a2cc414304c8389e1fa6811b0553b5e7d
Jay3404 <111181171+Jay3404@users.noreply.github.com>
2023-08-05 10:48:36 +0900

    feat-23: 첫 페이지 다음버튼 추가.

---
+++ src/components/join/Join.js
@@ -0,0 +1,61 @@
+import { Button } from '@mui/material';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
+
+const Join = ({ handleClick }) => {
+
+    const defaultTheme = createTheme();
+
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Typography sx={{ fontSize: '5px' }}
+                    >대충 회사 로고나 이름</Typography>
+                    <br></br>
+                    <br></br>
+                    <Typography component="h1" variant="h5" textAlign={'center'}>
+                        어흥! 반가워요!<br></br>
+                        당신에 대해 알려주면<br></br>
+                        제가 추천 해드릴게요!
+                    </Typography>
+                    <br></br>
+                    <br></br>
+
+                </Box>
+                <box sx={{ width: '100%', height: '100%', alignItems: 'center' }}>
+                    <img style={{ width: '100%', height: '100%' }} src={"https://search.pstatic.net/sunny/?src=https%3A%2F%2Fi.pinimg.com%2F736x%2F70%2Faa%2Fdb%2F70aadb580a93ca72f7b8591bf89df19d.jpg&type=a340"} />
+                </box>
+                <Button onClick={handleClick}
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    sx={{
+                        mt: 3,
+                        mb: 2,
+                        backgroundColor: '#FFB471', // 평소 색상
+                        '&:hover': {
+                            backgroundColor: '#E55C25', // 호버 시 색상
+                        },
+                    }}
+                >
+                    어흥 시작하기
+                </Button>
+            </Container>
+        </ThemeProvider>
+    );
+};
+
+export default Join;
