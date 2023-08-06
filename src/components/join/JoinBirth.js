87d974ae00e8b066af060e973519040f1f721592
DESKTOP-439URRO\Jay <the20027@naver.com>
2023-08-06 20:16:41 +0900

    feat-23: 생년월일 유효성 검사.

--- src/components/join/JoinBirth.js
+++ src/components/join/JoinBirth.js
@@ -4,24 +4,78 @@ import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import LinearProgress from '@mui/material/LinearProgress';
-import Link from '@mui/material/Link';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import React, { useEffect, useState } from 'react';
 import { styled } from '@mui/system';
+import React, { useEffect, useState } from 'react';
 import thumbImage from '../../public/image.png.png';
 
-const JoinBirth = () => {
+const JoinBirth = ({ handleClick, setUserBirth }) => {
     const [progress, setProgress] = useState(0);
+    const [checkBirth, setCheckBirth] = useState(true);
 
     const handleSubmit = (event) => {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
-        console.log({
-            email: data.get('email'),
-            password: data.get('password'),
-        });
+
+        var dateStr = data.get('birth');
+        var year = Number(dateStr.substr(0, 4)); // 입력한 값의 0~4자리까지 (연)
+        var month = Number(dateStr.substr(4, 2)); // 입력한 값의 4번째 자리부터 2자리 숫자 (월)
+        var day = Number(dateStr.substr(6, 2)); // 입력한 값 6번째 자리부터 2자리 숫자 (일)
+
+        const message = `생년월일이 ${year}년 ${month}월 ${day}일이 맞나요??`
+
+        if (window.confirm(message)) {
+            var today = new Date(); // 날짜 변수 선언
+            var yearNow = today.getFullYear(); // 올해 연도 가져옴
+
+            if (dateStr.length <= 8) {
+                // 연도의 경우 1900 보다 작거나 yearNow 보다 크다면 false를 반환합니다.
+                if (1900 > year || year > yearNow) {
+                    setCheckBirth(false);
+
+                } else if (month < 1 || month > 12) {
+                    setCheckBirth(false);
+
+                } else if (day < 1 || day > 31) {
+                    setCheckBirth(false);
+
+                } else if ((month === 4 || month === 6 || month === 9 || month === 11) && day === 31) {
+                    setCheckBirth(false);
+
+                } else if (month === 2) {
+
+                    var isleap = (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0));
+
+                    if (day > 29 || (day === 29 && !isleap)) {
+                        setCheckBirth(false);
+
+                    } else {
+                        setCheckBirth(true);
+                        setUserBirth(() => data.get('birth'));
+                        handleClick();
+                    }//end of if (day>29 || (day===29 && !isleap))
+
+                } else {
+                    setCheckBirth(true);
+                    setUserBirth(() => data.get('birth'));
+                    handleClick();
+                }//end of if
+
+            } else {
+                //1.입력된 생년월일이 8자 초과할때 :  auth:false
+                setCheckBirth(false);
+            }
+            //End of method /*
+
+
+        } else {
+            console.log("다시 입력해주세요");
+        }
+
+
+
     };
 
 
@@ -98,32 +152,19 @@ const JoinBirth = () => {
                     </Typography>
                     <br></br>
                     <br></br>
-                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3, width: '100%' }}>
+                    <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3, width: '100%' }}>
                         <Grid container spacing={2} >
                             <Grid item xs={12} >
                                 <TextField
                                     required
                                     fullWidth
-                                    id="code"
-                                    label="태어난 년도 (4자리 입력)"
-                                    name="code"
+                                    id="birth"
+                                    label="생년월일(8자리)을 입력해주세요."
+                                    placeholder='19600101'
+                                    name="birth"
                                     autoComplete="off"
-                                />
-                                <TextField
-                                    required
-                                    fullWidth
-                                    id="code"
-                                    label="태어난 월 (2자리 입력)"
-                                    name="code"
-                                    autoComplete="off"
-                                />
-                                <TextField
-                                    required
-                                    fullWidth
-                                    id="code"
-                                    label="태어난 일 (2자리 입력)"
-                                    name="code"
-                                    autoComplete="off"
+                                    error={!checkBirth}
+                                    helperText={!checkBirth && "생년월일 형식에 맞추어 입력해 주세요."}
                                 />
                             </Grid>
                         </Grid>
