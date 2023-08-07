c393a8cde85f61cf45d42199a4943d2567784df4
BBeedroid <maisy13@naver.com>
2023-08-07 15:10:17 +0900

    feat-42: 메인 캐러셀 기능 구현

--- src/components/partials/Layout.js
+++ src/components/partials/Layout.js
@@ -1,14 +1,10 @@
-import React from 'react'
-import styled from 'styled-components';
+import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../partials/Header';
 import Footer from '../partials/Footer';
 import Nav from '../partials/Nav';
 import Main from '../partials/Main';
 import '../../css/partials/Style.css';
-
-
-
 
 const Layout = () => {
     return (
@@ -24,7 +20,7 @@ const Layout = () => {
             <Footer></Footer>
 
         </>
-    )
-}
+    );
+};
 
-export default Layout
+export default Layout;
--- src/components/partials/Main.js
+++ src/components/partials/Main.js
@@ -1,71 +1,90 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 import '../../css/partials/Style.css';
+import Carousel from 'react-material-ui-carousel';
+import { Paper, Typography } from '@mui/material';
+import styled from 'styled-components';
 
 const Main = () => {
+    const [isDesktop, setIsDesktop] = useState(window.innerWidth > 712);
+
+    useEffect(() => {
+        const handleResize = () => setIsDesktop(window.innerWidth > 712);
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    const imageUrls = [
+        "https://i.postimg.cc/PrP5mdfb/001.png",
+        "https://i.postimg.cc/T2S6vgGm/002.png",
+        "https://i.postimg.cc/ZqMSYd31/003.png"
+    ];
+
+    const cardHeaders = ["화상채팅", "오늘의 생활팁", "소모임", "예시예시"];
+
+    const CardLink = styled(RouterLink)`
+        text-decoration: none;
+    `;
+
+    const CardTitle = styled.div`
+        display: inline-block;
+    `;
+
+    const StyledTypography = styled(Typography)`
+        color: #12406A;
+        cursor: pointer;
+
+        &:hover {
+            color: #FFB471;
+        }
+    `;
+
     return (
-        <div class="sb-nav-fixed mainpage">
+        <div className="sb-nav-fixed mainpage">
             <div id="layoutSidenav">
                 <div id="layoutSidenav_content">
                     <main>
-                        <div class="container-fluid px-4">
-                            <h1 class="mt-4">수정필요</h1>
-                            <ol class="breadcrumb mb-4">
-                                <li class="breadcrumb-item active">수정필요</li>
-                            </ol>
-                            <div class="row">
-                                <div class="col-xl-3 col-md-6">
-                                    <div class="card bg-primary text-white mb-4">
-                                        <div class="card-body">Primary Card</div>
-                                        <div class="card-footer d-flex align-items-center justify-content-between">
-                                            <a class="small text-white stretched-link" href="#">View Details</a>
-                                            <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                                        </div>
+                        <div className="container-fluid px-4">
+                            <div className="row">
+                                {isDesktop && (
+                                    <div className="carousel-container" style={{ marginBottom: "10px" }}>
+                                        <Carousel height={230} animation='slide' navButtonsAlwaysVisible='true' duration={3000}
+                                            indicators={true} indicatorContainerProps={{
+                                                style: {
+                                                    zIndex: 1,
+                                                    marginTop: "-15px",
+                                                    position: "relative"
+                                                }
+                                            }} sx={{ width: '100%', height: '100%', position: 'relative' }}>
+                                            {imageUrls.map((url, index) => (
+                                                <Paper key={index} sx={{ position: 'absolute', top: '55%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+                                                    <img src={url} />
+                                                </Paper>
+                                            ))}
+                                        </Carousel>
                                     </div>
-                                </div>
+                                )}
                             </div>
-                            <div class="row">
-                                <div class="col-xl-6">
-                                    <div class="card mb-4">
-                                        <div class="card-header">
-                                            <i class="fas fa-chart-area me-1"></i>
-                                            화상채팅
+                            <div className="row">
+                                {cardHeaders.map((header, index) => (
+                                    <div key={index} className="col-xl-6">
+                                        <div className="card mb-4">
+                                            <div className="card-header">
+                                                <CardTitle>
+                                                    <CardLink to="/">
+                                                        <StyledTypography variant="body1" fontWeight="bold">
+                                                            {header}
+                                                        </StyledTypography>
+                                                    </CardLink>
+                                                </CardTitle>
+                                            </div>
+                                            <div className="card-body"></div>
                                         </div>
-                                        <div class="card-body"></div>
                                     </div>
-                                </div>
-                                <div class="col-xl-6">
-                                    <div class="card mb-4">
-                                        <div class="card-header">
-                                            <i class="fas fa-chart-bar me-1"></i>
-                                            오늘의 생활지식
-                                        </div>
-                                        <div class="card-body"></div>
-                                    </div>
-                                </div>
-                                <div class="col-xl-6">
-                                    <div class="card mb-4">
-                                        <div class="card-header">
-                                            <i class="fas fa-chart-area me-1"></i>
-                                            소모임
-                                        </div>
-                                        <div class="card-body"></div>
-                                    </div>
-                                </div>
-                                <div class="col-xl-6">
-                                    <div class="card mb-4">
-                                        <div class="card-header">
-                                            <i class="fas fa-chart-area me-1"></i>
-                                            Area Chart Example
-                                        </div>
-                                        <div class="card-body"></div>
-                                    </div>
-                                </div>
+                                ))}
                             </div>
-
                         </div>
                     </main>
-
                 </div>
             </div>
         </div>
