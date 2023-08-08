8d7939387315405a01106b1fe9c17b11c5e0c625
parkseryu <pjh971212@gmail.com>
2023-08-08 16:41:29 +0900

    feat-66: 화상채팅 화면 구현

---
+++ src/components/CameraChatting.js
@@ -0,0 +1,266 @@
+import React, { useEffect, useRef, useState } from "react";
+import io from "socket.io-client";
+// import "../css/partials/Style.css";
+
+const CameraChatting = () => {
+  const [roomHidden, setRoomHidden] = useState(false);
+  const [test, setTest] = useState(0);
+  const [isMuted, setIsMuted] = useState(false);
+  const [isCameraOff, setIsCameraOff] = useState(false);
+  const [myStreamState, setMyStreamState] = useState(null);
+  const [selectedCamera, setSelectedCamera] = useState(null);
+
+  const roomNameRef = useRef(null);
+  const myPeerConnection = useRef(null); // useRef는 연결 객체가 변경될 때마다 컴포넌트를 리렌더링하지 않도록 하기 위해 사용됩니다.
+  const myDataChannel = useRef(null);
+  const myStreamRef = useRef(null);
+  const myFaceRef = useRef(null);
+  const cameraSelectRef = useRef(null);
+  const peerFaceRef = useRef(null);
+  const socket = useRef();
+
+  useEffect(() => {
+    socket.current = io("http://localhost:5000");
+
+    socket.current.on("welcome", async () => {
+      // myDataChannel.current =
+      //   myPeerConnection.current.createDataChannel("chat");
+      // myDataChannel.current.addEventListener("message", (event) => {
+      //   console.log(event.data);
+      // });
+      // console.log("made data channel");
+      const offer = await myPeerConnection.current.createOffer();
+      // console.log(`offer : ${offer}`);
+      myPeerConnection.current.setLocalDescription(offer);
+      console.log("sent the offer");
+      socket.current.emit("offer", offer, roomNameRef.current);
+    });
+
+    socket.current.on("offer", async (offer) => {
+      // myPeerConnection.current.addEventListener("datachannel", (event) => {
+      //   myDataChannel.current = event.channel;
+      //   myDataChannel.current.addEventListener("message", (event) => {
+      //     console.log(event);
+      //   });
+      // });
+      console.log("received the offer");
+      myPeerConnection.current.setRemoteDescription(offer);
+      const answer = await myPeerConnection.current.createAnswer();
+      myPeerConnection.current.setLocalDescription(answer);
+      socket.current.emit("answer", answer, roomNameRef.current);
+      console.log("sent the answer");
+    });
+
+    socket.current.on("answer", (answer) => {
+      console.log("received the answer");
+      myPeerConnection.current.setRemoteDescription(answer);
+    });
+
+    socket.current.on("ice", (ice) => {
+      console.log(`received candidate `);
+      // console.log(ice)
+      myPeerConnection.current.addIceCandidate(ice);
+    });
+
+    // 클린업 (component unmount 또는 dependencies 변경 시 실행됨)
+    return () => {
+      socket.current.off("welcome");
+      socket.current.off("offer");
+      socket.current.off("answer");
+      socket.current.off("ice");
+    };
+  }, []); // 빈 배열은 이 효과가 컴포넌트 마운트 시 한 번만 실행되게 함
+
+  const makeConnection = () => {
+    myPeerConnection.current = new RTCPeerConnection({
+      iceServers: [
+        {
+          urls: [
+            "stun:stun.l.google.com:19302",
+            "stun:stun1.l.google.com:19302",
+            "stun:stun2.l.google.com:19302",
+            "stun:stun3.l.google.com:19302",
+            "stun:stun4.l.google.com:19302",
+          ],
+        },
+      ],
+    });
+    myPeerConnection.current.addEventListener("icecandidate", handleIce);
+    myPeerConnection.current.addEventListener("track", handleTrack);
+    myStreamRef.current
+      .getTracks()
+      .forEach((track) =>
+        myPeerConnection.current.addTrack(track, myStreamRef.current)
+      );
+  };
+
+  const handleIce = (data) => {
+    console.log("sent candidate");
+    socket.current.emit("ice", data.candidate, roomNameRef.current);
+  };
+
+  const handleTrack = (event) => {
+    const stream = event.streams[0];
+    peerFaceRef.current.srcObject = stream;
+  };
+
+  const getCameras = async () => {
+    try {
+      const deviceList = await navigator.mediaDevices.enumerateDevices();
+      const cameraList = deviceList.filter(
+        (device) => device.kind === "videoinput"
+      );
+
+      if (myStreamRef.current) {
+        const currentCamera = myStreamRef.current.getVideoTracks()[0];
+        cameraList.forEach((camera) => {
+          const option = document.createElement("option");
+          option.value = camera.deviceId;
+          option.innerText = camera.label;
+          if (currentCamera && currentCamera.label === camera.label) {
+            option.selected = true;
+          }
+          if (cameraSelectRef.current) {
+            cameraSelectRef.current.appendChild(option);
+          }
+        });
+      }
+      // console.log(deviceList);
+      // console.log(cameraList);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const getMedia = async (deviceId) => {
+    const initialConstrains = {
+      audio: true,
+      video: { facingMode: "user" },
+    };
+    const cameraConstraints = {
+      audio: true,
+      video: { deviceId: { exact: deviceId } },
+    };
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia(
+        deviceId ? cameraConstraints : initialConstrains
+      );
+
+      myStreamRef.current = stream;
+
+      if (myFaceRef.current) {
+        myFaceRef.current.srcObject = stream;
+      }
+
+      await getCameras();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const initCall = async () => {
+    setRoomHidden(true);
+    await getMedia();
+    makeConnection();
+  };
+
+  const handleRoomSubmit = async (e) => {
+    e.preventDefault();
+    await initCall();
+    roomNameRef.current = e.target.roomName.value;
+    socket.current.emit("join_room", roomNameRef.current);
+  };
+
+  const handleMuteClick = () => {
+    if (!myStreamRef.current) return;
+
+    const audioTracks = myStreamRef.current.getAudioTracks();
+    audioTracks.forEach((track) => (track.enabled = !track.enabled));
+
+    setIsMuted((prevIsMuted) => !prevIsMuted);
+  };
+
+  const handleCameraOnOff = () => {
+    if (!myStreamRef.current) return;
+
+    const videoTracks = myStreamRef.current.getVideoTracks();
+    videoTracks.forEach((track) => (track.enabled = !track.enabled));
+
+    setIsCameraOff((prevIsCameraOff) => !prevIsCameraOff);
+  };
+
+  const handleCameraChange = async (e) => {
+    const cameraValue = e.target.value;
+    setSelectedCamera(cameraValue);
+
+    const newStream = await getMedia(cameraValue);
+    setMyStreamState(newStream);
+
+    if (myPeerConnection) {
+      console.log(myPeerConnection.getSenders());
+      const videoTrack = newStream.getVideoTracks()[0];
+      const videoSender = myPeerConnection
+        .getSenders()
+        .find((sender) => sender.track.kind === "video");
+
+      videoSender.replaceTrack(videoTrack);
+    }
+  };
+
+  return (
+    <div>
+      <div class="sb-nav-fixed mainpage">
+        <div id="layoutSidenav">
+          <div id="layoutSidenav_content">
+            <main>
+              <div class="container-fluid px-4">
+                <div id="welcome" hidden={roomHidden}>
+                  <form onSubmit={handleRoomSubmit}>
+                    <input
+                      placeholder="room name"
+                      name="roomName"
+                      required
+                      type="text"
+                    />
+                    <button>Enter room</button>
+                  </form>
+                </div>
+                <div id="call" hidden={!roomHidden}>
+                  <div id="myStreamState">
+                    <video
+                      ref={myFaceRef}
+                      muted
+                      autoPlay
+                      playsInline
+                      width="400"
+                      height="400"
+                    />
+                    <button onClick={() => handleMuteClick()}>
+                      {isMuted ? "UnMute" : "Mute"}
+                    </button>
+                    <button onClick={() => handleCameraOnOff()}>
+                      {isCameraOff ? "Turn Camera On" : "Turn Camera Off"}
+                    </button>
+                    {/* <select
+                      ref={cameraSelectRef}
+                      onChange={() => handleCameraChange()}
+                    /> */}
+                    <video
+                      ref={peerFaceRef}
+                      autoPlay
+                      playsInline
+                      width="400"
+                      height="400"
+                    />
+                  </div>
+                </div>
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CameraChatting;
