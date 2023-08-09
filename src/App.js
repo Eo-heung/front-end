e4e04a07947cf3c967c5c1af739544bfab3d7224
zener9233 <cite9233@naver.com>
2023-08-09 09:36:54 +0900

    feat-7: 로그인 화면 접속 시 ACCESS_TOKEN 서치 후 토큰 존재 시 로그인 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Password from './components/Password';
+import SuccessPage from './components/SuccessPage';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path='/login' element={<Login></Login>}></Route>
       <Route path='/findpassword' element={<Password></Password>}></Route>
+      <Route path='/success' element={<SuccessPage></SuccessPage>} />
     </Routes>
   );
 }
