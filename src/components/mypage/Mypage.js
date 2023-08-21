2b6a9b96f4313fb0b33990fbf34a595ab8584365
zener9233 <cite9233@naver.com>
2023-08-21 15:56:02 +0900

    feat-46: 수락된 친구 목록 구현

--- src/components/mypage/Mypage.js
+++ src/components/mypage/Mypage.js
@@ -86,6 +86,7 @@ const Mypage = () => {
     const checkPasswordRef = createRef();
     const [imageFile, setImageFile] = useState(null);
     const [requestFriends, setRequestFriends] = useState([]);
+    const [friends, setFriends] = useState([]);
     const [isPasswordModalOpen, setPasswordModalOpen] = useState(false);
 
     const openModal = () => {
@@ -630,17 +631,18 @@ const Mypage = () => {
 
     // 친구 관리하기
 
+
+
+
     const getFriendList = async () => {
         axios.post('http://localhost:9000/friend/friendList', {}, {
             headers: {
                 Authorization: `Bearer ${sessionStorage.getItem("ACCESS_TOKEN")}`
             }
         }).then(res => {
-            console.log("-----------friendListfriendListfriendList-----------------")
-            console.log(res.data);
+            setFriends(res.data.items);
         }).catch(error => { console.error(error) });
     };
-
 
     const getRequestFriendList = async () => {
         axios.post('http://localhost:9000/friend/requestFriendList', {}, {
@@ -1086,35 +1088,23 @@ const Mypage = () => {
                                     </Tabs>
                                     <TabPanel1 value={value1} index={0}>
                                         <Grid container spacing={3} sx={{ marginTop: '5px' }}>
-                                            <Grid item xs={12}>
-                                                <BoxContent sx={{ padding: '16px', boxShadow: '0px 1px 3px rgba(0, 0, 0, 0.2)' }}>
-                                                    <div style={{ display: 'flex', alignItems: 'center', marginBottom: '8px' }}>
-                                                        <img
-                                                            src="https://cdnimg.melon.co.kr/cm2/artistcrop/images/002/61/143/261143_20210325180240_500.jpg?61e575e8653e5920470a38d1482d7312/melon/resize/416/quality/80/optimize"
-                                                            alt="프로필 사진"
-                                                            style={{ width: '50px', height: '50px', borderRadius: '25px', marginRight: '8px' }}
-                                                        />
-                                                        <Typography variant="h6">친구닉네임</Typography>
-                                                    </div>
-                                                    <Typography variant="body2" sx={{ marginBottom: '6px' }}>지역 : 역삼1동</Typography>
-                                                    <Typography variant="body2" sx={{ marginBottom: '6px' }}>관심사: hobby1</Typography>
-                                                    <Typography variant="body2">상태메세지: 상태메세지내용</Typography>
-                                                </BoxContent>
-                                            </Grid>
-                                            <Grid item xs={12}>
-                                                <BoxContent sx={{ padding: '16px', boxShadow: '0px 1px 3px rgba(0, 0, 0, 0.2)' }}>
-                                                    <div style={{ display: 'flex', alignItems: 'center', marginBottom: '8px' }}>
-                                                        <img
-                                                            src="https://cdnimg.melon.co.kr/cm2/artistcrop/images/002/61/143/261143_20210325180240_500.jpg?61e575e8653e5920470a38d1482d7312/melon/resize/416/quality/80/optimize"
-                                                            alt="프로필 사진"
-                                                            style={{ width: '50px', height: '50px', borderRadius: '25px', marginRight: '8px' }}
-                                                        />
-                                                        <Typography variant="h6">친구닉네임</Typography>
-                                                    </div>
-                                                    <Typography variant="body2" sx={{ marginBottom: '6px' }}>지역 : 역삼1동</Typography>
-                                                    <Typography variant="body2" sx={{ marginBottom: '6px' }}>관심사: hobby1</Typography>
-                                                    <Typography variant="body2">상태메세지: 상태메세지내용</Typography>
-                                                </BoxContent>
+                                            <Grid item xs={12} >
+                                                {friends.map((friend, index) => (
+                                                    <Grid item xs={12} key={index}>
+                                                        <BoxContent sx={{ padding: '16px', boxShadow: '0px 1px 3px rgba(0, 0, 0, 0.2)' }}>
+                                                            <div style={{ display: 'flex', alignItems: 'center', marginBottom: '8px' }}>
+                                                                <img
+                                                                    src={friend.profile ? `data:image/jpeg;base64,${friend.profile}` : "https://cdnimg.melon.co.kr/cm2/artistcrop/images/002/61/143/261143_20210325180240_500.jpg?61e575e8653e5920470a38d1482d7312/melon/resize/416/quality/80/optimize"}
+                                                                    alt="프로필 사진"
+                                                                    style={{ width: '50px', height: '50px', borderRadius: '25px', marginRight: '8px' }}
+                                                                />
+                                                                <Typography variant="h6">{friend.user_name || "이름 없음"}</Typography>
+                                                            </div>
+                                                            <Typography variant="body2" sx={{ marginBottom: '6px' }}>지역 : {friend.user_addr3 || "지역 없음"}</Typography>
+                                                            <Typography variant="body2">상태메세지: {friend.user_status_message || "상태 메세지 없음"}</Typography>
+                                                        </BoxContent>
+                                                    </Grid>
+                                                ))}
                                             </Grid>
                                         </Grid>
                                     </TabPanel1>
