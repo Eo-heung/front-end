344d5b162cfa798d59ea7a39d24fe691e0dd4a42
jmeancode <seongjimin199@gmail.com>
2023-08-26 19:12:26 +0900

    feat-66: 랜덤채팅 결제 팝업 생성, 메세지 알람 기능 추가

--- src/components/chatting/CameraChatting.js
+++ src/components/chatting/CameraChatting.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState, useCallback } from "react";
 import io from "socket.io-client";
 import axios from "axios";
 import MicIcon from "@mui/icons-material/Mic";
@@ -31,13 +31,28 @@ const CameraChatting = ({ selectedCamera, selectedMic }) => {
   const [typingUsers, setTypingUsers] = useState([]);
   const socket = useRef();
   const [roomName, setRoomName] = useState("");
+  const newWindowRef = useRef(null);
+  const [showNotification, setShowNotification] = useState(false);
+  const textChatVisibleRef = useRef(textChatVisible);
+  const chatIcon = textChatVisible ? (
+    <SpeakerNotesOffIcon />
+  ) : (
+    <SpeakerNotesIcon />
+  );
+  const notificationIndicator = showNotification ? (
+    <div className="notification-circle"></div>
+  ) : null;
 
   useEffect(() => {
     scrollToBottom();
   }, [messages, textChatVisible]);
 
   useEffect(() => {
-    socket.current = io("http://localhost:9000");
+    textChatVisibleRef.current = textChatVisible;
+  }, [textChatVisible]);
+
+  useEffect(() => {
+    socket.current = io("http://localhost:4000");
     setMyNickname(getCookie("userNickname"));
     fetchNickname(); // 여기서 닉네임을 가져옴
 
@@ -91,6 +106,10 @@ const CameraChatting = ({ selectedCamera, selectedMic }) => {
           ...prevMessages,
           { content: message, type: "received" },
         ]);
+        // 채팅창이 닫혀 있을 때만 알림 동그라미를 띄운다.
+        if (!textChatVisible) {
+          setShowNotification(true);
+        }
       });
 
       console.log("made data channel");
@@ -103,15 +122,14 @@ const CameraChatting = ({ selectedCamera, selectedMic }) => {
     socket.current.on("offer", async (offer) => {
       myPeerConnection.current.addEventListener("datachannel", (event) => {
         myDataChannel.current = event.channel;
-        myDataChannel.current.addEventListener("message", (event) => {
-          const message = event.data;
-          console.log("Received from offer:", message);
-          setMessages((prevMessages) => [
-            ...prevMessages,
-            { content: message, type: "received" },
-          ]);
-        });
+
+        // 기존 리스너 제거 (이중 리스너 추가 방지)
+        myDataChannel.current.removeEventListener("message", onMessage);
+
+        // 새로운 리스너 추가
+        myDataChannel.current.addEventListener("message", onMessage);
       });
+
       console.log("received the offer");
       await myPeerConnection.current.setRemoteDescription(offer);
 
@@ -312,7 +330,7 @@ const CameraChatting = ({ selectedCamera, selectedMic }) => {
 
   async function fetchNickname() {
     try {
-      const response = await axios.get("http://localhost:5000/nickname", {
+      const response = await axios.get("http://localhost:4000/nickname", {
         params: {
           nickname: userNickname,
         },
@@ -326,6 +344,42 @@ const CameraChatting = ({ selectedCamera, selectedMic }) => {
   }
   const handleMessageInput = () => {
     socket.current.emit("typing", roomName);
+  };
+  const handleInputFocus = () => {
+    setShowNotification(false);
+  };
+
+  const handlechargeClick = useCallback(() => {
+    newWindowRef.current = window.open(
+      "http://localhost:1234/charge",
+      "_blank",
+      "width=800,height=600"
+    );
+  }, []);
+
+  const onMessage = (event) => {
+    const message = event.data;
+    console.log("Received from offer:", message);
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { content: message, type: "received" },
+    ]);
+
+    if (!textChatVisible) {
+      setShowNotification(true);
+    }
+  };
+
+  const handleChatButtonClick = () => {
+    console.log("Before:", textChatVisible, showNotification);
+
+    // 먼저 textChatVisible의 상태를 변경
+    setTextChatVisible((prev) => !prev);
+
+    // 그 다음, 변경된 상태에 따라 showNotification 상태를 변경
+    if (!textChatVisible) {
+      setShowNotification(false);
+    }
   };
 
   return (
@@ -392,13 +446,16 @@ const CameraChatting = ({ selectedCamera, selectedMic }) => {
           >
             정지
           </button>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => setTextChatVisible(!textChatVisible)}
-          >
-            {!textChatVisible ? <SpeakerNotesIcon /> : <SpeakerNotesOffIcon />}
-          </Button>
+          <div className="chat-button-wrapper">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleChatButtonClick}
+            >
+              {chatIcon}
+              {notificationIndicator}
+            </Button>
+          </div>
           <Button variant="contained" color="primary" onClick={handleMuteClick}>
             {isMuted ? <MicOffIcon /> : <MicIcon />}
           </Button>
@@ -416,9 +473,9 @@ const CameraChatting = ({ selectedCamera, selectedMic }) => {
           <Button
             variant="contained"
             color="primary"
-            onClick={handleCameraOnOff}
+            onClick={handlechargeClick}
           >
-            {isCameraOff ? "곧 감" : "너에게"}
+            곶감충전
           </Button>
         </div>
         {textChatVisible && (
@@ -447,6 +504,7 @@ const CameraChatting = ({ selectedCamera, selectedMic }) => {
                 name="message"
                 placeholder="메세지를 입력해주세요."
                 onKeyDown={handleMessageInput}
+                onFocus={handleInputFocus}
               />
               <Button variant="contained" color="primary" type="submit">
                 보내기
