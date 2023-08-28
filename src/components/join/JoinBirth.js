39fce115141442bda824c8c336814e63051e591c
DESKTOP-439URRO\Jay <the20027@naver.com>
2023-08-29 00:49:27 +0900

    design-23: 회원가입 화면 디자인 수정

--- src/components/join/JoinBirth.js
+++ src/components/join/JoinBirth.js
@@ -1,3 +1,5 @@
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import { IconButton } from '@mui/material';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
@@ -13,12 +15,55 @@ import Typography from '@mui/material/Typography';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { styled } from '@mui/system';
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
 import thumbImage from '../../public/01.png';
 
-const JoinBirth = ({ handleClick, setUserBirth, setUserGender }) => {
+// 원의 left 값을 progress에 바인딩하기 위해 styled 컴포넌트 대신 일반 함수 컴포넌트를 사용합니다.
+const Circle = styled('div')(({ progress }) => ({
+    position: 'absolute',
+    left: `calc(${progress}% - 5px)`,
+    top: '50%',
+    transform: 'translateY(-50%)',
+    width: '40px',
+    height: '40px',
+    backgroundImage: `url(${thumbImage})`, // 가져온 이미지를 배경 이미지로 사용합니다.
+    backgroundSize: 'cover', // 필요에 따라 배경 이미지 크기를 조절합니다.
+    zIndex: 2,
+    transition: "left 500ms ease-out"
+}));
+
+function LinearProgressWithLabel() {
+    const [progress, setProgress] = useState(50);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setProgress(66.666);
+        }, 500);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, []);
+
+
+    return (
+        <Box sx={{ position: 'relative', display: 'flex', alignItems: 'center', width: '100%', height: '20px' }}>
+            <Box sx={{ position: 'relative', flex: 14, marginRight: "10px" }}>
+                <LinearProgress variant="determinate" value={progress} />
+                <Circle progress={progress} />
+            </Box>
+            <Box sx={{ flex: 1, marginLeft: 3 }}>
+                <Typography variant="body2" color="black" sx={{ width: '30px' }}>{'4 / 6'}</Typography>
+            </Box>
+        </Box>
+    );
+}
+
+const JoinBirth = ({ handleClick, setUserBirth, setUserGender, backClick }) => {
     const [progress, setProgress] = useState(0);
     const [checkBirth, setCheckBirth] = useState(true);
-    const [gender, setGender] = useState('0');
+    const [gender, setGender] = useState('1');
+    const navi = useNavigate();
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -34,6 +79,12 @@ const JoinBirth = ({ handleClick, setUserBirth, setUserGender }) => {
         if (window.confirm(message)) {
             var today = new Date(); // 날짜 변수 선언
             var yearNow = today.getFullYear(); // 올해 연도 가져옴
+
+            if (yearNow - year < 60) {
+                setCheckBirth(false);
+                alert("만 60세가 넘어야 가입이 가능합니다.");
+                navi('/login');
+            }
 
             if (dateStr.length <= 8) {
                 // 연도의 경우 1900 보다 작거나 yearNow 보다 크다면 false를 반환합니다.
@@ -69,66 +120,18 @@ const JoinBirth = ({ handleClick, setUserBirth, setUserGender }) => {
                     setUserGender(() => data.get('gender'));
                     handleClick();
                 }//end of if
-
             } else {
                 //1.입력된 생년월일이 8자 초과할때 :  auth:false
                 setCheckBirth(false);
             }
             //End of method /*
-
-
         } else {
             console.log("다시 입력해주세요");
         }
 
-
-
     };
 
-
-
-    // 원의 left 값을 progress에 바인딩하기 위해 styled 컴포넌트 대신 일반 함수 컴포넌트를 사용합니다.
-    const Circle = styled('div')(({ progress }) => ({
-        position: 'absolute',
-        left: `calc(${progress}% - 5px)`,
-        top: '50%',
-        transform: 'translateY(-50%)',
-        width: '40px',
-        height: '40px',
-        backgroundImage: `url(${thumbImage})`, // 가져온 이미지를 배경 이미지로 사용합니다.
-        backgroundSize: 'cover', // 필요에 따라 배경 이미지 크기를 조절합니다.
-        zIndex: 2,
-        transition: "left 500ms ease-out"
-    }));
-
     const defaultTheme = createTheme();
-
-    function LinearProgressWithLabel() {
-        const [progress, setProgress] = useState(50);
-
-        useEffect(() => {
-            const timer = setTimeout(() => {
-                setProgress(66.666);
-            }, 500);
-
-            return () => {
-                clearTimeout(timer);
-            };
-        }, []);
-
-
-        return (
-            <Box sx={{ position: 'relative', display: 'flex', alignItems: 'center', width: '100%', height: '20px' }}>
-                <Box sx={{ position: 'relative', flex: 14, marginRight: "10px" }}>
-                    <LinearProgress variant="determinate" value={progress} />
-                    <Circle progress={progress} />
-                </Box>
-                <Box sx={{ flex: 1, marginLeft: 3 }}>
-                    <Typography variant="body2" color="black" sx={{ width: '30px' }}>{'4 / 6'}</Typography>
-                </Box>
-            </Box>
-        );
-    }
 
     const theme = createTheme({
         palette: {
@@ -138,18 +141,29 @@ const JoinBirth = ({ handleClick, setUserBirth, setUserGender }) => {
         },
     });
 
-
     return (
         <ThemeProvider theme={defaultTheme}>
             <Container component="main" maxWidth="xs" style={{ overflow: 'hidden' }}>
                 <CssBaseline />
                 <Box
                     sx={{
+                        position: 'relative', // 추가
                         minHeight: '608.57px',
                         maxHeight: '608.57px',
                         marginTop: 12.5
                     }}
                 >
+                    <IconButton
+                        sx={{
+                            position: 'absolute',
+                            top: "-70px",
+                        }}
+                        onClick={() => {
+                            backClick();
+                        }}
+                    >
+                        <ArrowBackIosIcon />
+                    </IconButton>
                     <Typography variant="h5" fontSize="12pt" gutterBottom textAlign={'center'} style={{ fontFamily: "font-medium", color: 'gray' }}>
                         어흥
                     </Typography>
@@ -183,8 +197,8 @@ const JoinBirth = ({ handleClick, setUserBirth, setUserGender }) => {
                                         onChange={(event) => setGender(event.target.value)}
 
                                     >
-                                        <FormControlLabel value='1' control={<Radio />} label="여성" />
-                                        <FormControlLabel value="0" control={<Radio />} label="남성" />
+                                        <FormControlLabel value='1' control={<Radio />} label="남성" />
+                                        <FormControlLabel value="0" control={<Radio />} label="여성" />
                                     </RadioGroup>
                                 </FormControl>
                             </Grid>
@@ -206,6 +220,7 @@ const JoinBirth = ({ handleClick, setUserBirth, setUserGender }) => {
                                 sx={{
                                     color: 'black',
                                     height: '44px',
+                                    fontFamily: "font-medium",
                                     mt: 3,
                                     mb: 2,
                                     backgroundColor: '#FEA53D', // 평소 색상
