56115206baae37cf7a9b69def46084c07888ab33
zener9233 <cite9233@naver.com>
2023-08-29 17:34:08 +0900

    feat: localhost수정

--- src/components/socialLogin/KakaoHandler.js
+++ src/components/socialLogin/KakaoHandler.js
@@ -1,10 +1,24 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import { useCookies } from "react-cookie";
 import { SPRING_API_URL, REDIRECT_URL } from "../../config";
 
 
+
+
+
 const KakaoHandler = () => {
+    const [cookies, setCookie] = useCookies(["userNickname", "userId"]);
+    const loginSuccessHandler = (data) => {
+        console.log("Received data:", data);
+        if (data.userName) {
+            setCookie('userNickname', data.userName, { path: '/' });
+        }
+        if (data.userId) {
+            setCookie('userId', data.userId, { path: '/' });
+        }
+    };
     const navi = useNavigate();
     const grant_type = "authorization_code";
     const client_id = 'd85c142dc0c92939902ad3248688e8ad';
@@ -40,21 +54,29 @@ const KakaoHandler = () => {
                             }
                         }
                     )
+
                         .then((res) => {
                             console.log("데이터 성공 :")
                             console.log(res);
+                            const currentDate = new Date();
+                            const currentYear = currentDate.getFullYear();
+
                             axios.post(`${SPRING_API_URL}/kakaoLogin`, {
                                 userId: res.data.kakao_account.email,
-                                userBirth: res.data.kakao_account.birthday,
-                                userNickname: res.data.kakao_account.nickname
-
+                                userBirth: (currentYear - res.data.kakao_account.age_range.split("~").map(Number)[0]) + res.data.kakao_account.birthday,
+                                userName: res.data.kakao_account.profile.nickname,
+                                userGender: res.data.kakao_account.gender === "male" ? 1 : 0,
+                                userEmail: res.data.kakao_account.email,
                             })
                                 .then((res) => {
+
                                     alert(`${res.data.item.userName}님 환영합니다.`);
                                     localStorage.setItem("REFRESH_TOKEN", res.data.item.token);
                                     sessionStorage.setItem("ACCESS_TOKEN", res.data.item.token);
                                     sessionStorage.setItem("userId", res.data.item.userId);
+                                    loginSuccessHandler(res.data.item);
                                     navi("/");
+
                                 })
 
                         });
