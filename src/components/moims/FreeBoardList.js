c97010e33e372f38887876a7872552ff4ff9684d
BBeedroid <maisy13@naver.com>
2023-08-30 12:35:12 +0900

    feat-77: 게시판 목록 검색, 공지 목록 글쓰기 제한 기능 구현 및 백엔드 연결 확인

--- src/components/moims/FreeBoardList.js
+++ src/components/moims/FreeBoardList.js
@@ -1,18 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
-import { StyledBasicContainer, StyledPaper, StyledContainer, Styled, StyledHead, StyledRow, StyledCell, StyledHeaderCell, StyledMainHeaderCell, StyledText } from '../utils/StyledTable';
+import { StyledBasicContainer, StyledPaper, StyledContainer, Styled, StyledHead, StyledRow, StyledCell, StyledHeaderCell, StyledMainHeaderCell, StyledText, StyledFooter } from '../utils/StyledTable';
 import { stubFalse } from 'lodash';
 import { SPRING_API_URL } from '../../config';
 import ListPagination from '../utils/Pagination';
+import { ListMoimSearchContainer, ListMoimTextField, ListMoimSelect, ListMoimMenuItem, ListMoimSearchButton } from '../utils/StyledListMoim';
 
 const FreeBoardList = ({ isMainPage = stubFalse, setActiveTab, setBoardType, setBoardId }) => {
     const navi = useNavigate();
 
     const { moimId } = useParams();
-    console.log(moimId);
 
     const [boards, setBoards] = useState([]);
+
+    const [page, setPage] = useState(1);
+    const [isLastPage, setIsLastPage] = useState(false);
+    const [keyword, setKeyword] = useState("");
+    const [searchType, setSearchType] = useState("all");
+    const [orderBy, setOrderBy] = useState("descending");
+
+    const fetchFreeBoards = async () => {
+        try {
+            const response = await axios.post(`${SPRING_API_URL}/board/${moimId}/free-board`, {}, {
+                headers: {
+                    Authorization: `Bearer ${sessionStorage.getItem("ACCESS_TOKEN")}`
+                },
+                params: {
+                    page: page - 1,
+                    keyword: keyword,
+                    searchType: searchType,
+                    orderBy: orderBy
+                }
+            });
+
+            setBoards(response.data.content);
+            setBoardType(response.data.content.boardType);
+        } catch (err) {
+            console.error("Error fetching free boards", err);
+        }
+    };
 
     useEffect(() => {
         if (!moimId || moimId == "undefined") {
@@ -20,25 +47,15 @@ const FreeBoardList = ({ isMainPage = stubFalse, setActiveTab, setBoardType, set
             return;
         }
 
-        console.log("moimId", moimId);
-
-        const fetchFreeBoards = async () => {
-            try {
-                const response = await axios.post(`${SPRING_API_URL}/board/${moimId}/free-board`, {}, {
-                    headers: {
-                        Authorization: `Bearer ${sessionStorage.getItem("ACCESS_TOKEN")}`
-                    }
-                });
-                setBoards(response.data.content);
-                setBoardType(response.data.content.boardType);
-                console.log("하이하이", response.data.content);
-            } catch (err) {
-                console.error("Error fetching free boards", err);
-            }
-        };
-
         fetchFreeBoards();
     }, [moimId]);
+
+    const handleSearch = () => {
+        setPage(1);
+        setBoards([]);
+        fetchFreeBoards();
+        console.log("검색", boards);
+    };
 
     return (
         <StyledBasicContainer>
@@ -90,7 +107,34 @@ const FreeBoardList = ({ isMainPage = stubFalse, setActiveTab, setBoardType, set
                         </tbody>
                     </Styled>
                     {!isMainPage ? (
-                        <ListPagination></ListPagination>
+                        <StyledFooter>
+                            <ListPagination></ListPagination>
+                            <ListMoimSearchContainer>
+                                <ListMoimTextField
+                                    style={{ marginLeft: "5rem" }}
+                                    size="small"
+                                    variant="outlined"
+                                    placeholder="검색어를 입력하세요."
+                                    onChange={(e) => setKeyword(e.target.value)} />
+                                <ListMoimSelect
+                                    value={searchType}
+                                    displayEmpty
+                                    size="small"
+                                    onChange={(e) => setSearchType(e.target.value)}>
+                                    <ListMoimMenuItem value="all">전체</ListMoimMenuItem>
+                                    <ListMoimMenuItem value="title">제목</ListMoimMenuItem>
+                                    <ListMoimMenuItem value="content">내용</ListMoimMenuItem>
+                                    <ListMoimMenuItem value="nickname">작성자</ListMoimMenuItem>
+                                </ListMoimSelect>
+                                <ListMoimSearchButton variant="contained" size="normal" onClick={handleSearch}>검색</ListMoimSearchButton>
+                                <ListMoimSearchButton
+                                    variant="contained"
+                                    size="normal"
+                                    onClick={() => navi(`/${moimId}/create-board`, { state: { boardType: "FREE" } })}>
+                                    글쓰기
+                                </ListMoimSearchButton>
+                            </ListMoimSearchContainer>
+                        </StyledFooter>
                     ) : null}
                 </StyledContainer>
             </StyledPaper>
--- src/components/moims/MoimNoticeList.js
+++ src/components/moims/MoimNoticeList.js
@@ -1,16 +1,63 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
-import { StyledBasicContainer, StyledPaper, StyledContainer, Styled, StyledHead, StyledRow, StyledCell, StyledHeaderCell, StyledMainHeaderCell, StyledText } from '../utils/StyledTable';
+import { StyledBasicContainer, StyledPaper, StyledContainer, Styled, StyledHead, StyledRow, StyledCell, StyledHeaderCell, StyledMainHeaderCell, StyledText, StyledFooter } from '../utils/StyledTable';
 import { stubFalse } from 'lodash';
 import { SPRING_API_URL } from '../../config';
+import ListPagination from '../utils/Pagination';
+import { ListMoimSearchContainer, ListMoimTextField, ListMoimSelect, ListMoimMenuItem, ListMoimSearchButton } from '../utils/StyledListMoim';
 
 const MoimNoticeList = ({ isMainPage = stubFalse, setActiveTab, setBoardType, setBoardId }) => {
     const navi = useNavigate();
 
     const { moimId } = useParams();
+    const [userRole, setUserRole] = useState({ isMember: false, isLeader: false });
 
     const [notices, setNotices] = useState([]);
+
+    const [page, setPage] = useState(1);
+    const [isLastPage, setIsLastPage] = useState(false);
+    const [keyword, setKeyword] = useState("");
+    const [searchType, setSearchType] = useState("all");
+    const [orderBy, setOrderBy] = useState("descending");
+
+    useEffect(() => {
+        const verifyUserRole = async () => {
+            try {
+                const response = await axios.post(`${SPRING_API_URL}/board/${moimId}/verify-role`, {}, {
+                    headers: {
+                        Authorization: `Bearer ${sessionStorage.getItem("ACCESS_TOKEN")}`
+                    }
+                });
+
+                setUserRole(response.data.item);
+            } catch (err) {
+                console.error("Error verifying user role", err);
+            }
+        };
+
+        verifyUserRole();
+    }, [moimId]);
+
+    const fetchNotices = async () => {
+        try {
+            const response = await axios.post(`${SPRING_API_URL}/board/${moimId}/notice-board`, {}, {
+                headers: {
+                    Authorization: `Bearer ${sessionStorage.getItem("ACCESS_TOKEN")}`
+                },
+                params: {
+                    page: page - 1,
+                    keyword: keyword,
+                    searchType: searchType,
+                    orderBy: orderBy
+                }
+            });
+            setNotices(response.data.content);
+            setBoardType(response.data.content.boardType);
+        } catch (err) {
+            console.error("Error fetching moim notices", err);
+        }
+    };
 
     useEffect(() => {
         if (!moimId || moimId == "undefined") {
@@ -18,22 +65,15 @@ const MoimNoticeList = ({ isMainPage = stubFalse, setActiveTab, setBoardType, se
             return;
         }
 
-        const fetchNotices = async () => {
-            try {
-                const response = await axios.post(`${SPRING_API_URL}/board/${moimId}/notice-board`, {}, {
-                    headers: {
-                        Authorization: `Bearer ${sessionStorage.getItem("ACCESS_TOKEN")}`
-                    }
-                });
-                setNotices(response.data.content);
-                setBoardType(response.data.content.boardType);
-            } catch (err) {
-                console.error("Error fetching moim notices", err);
-            }
-        };
-
         fetchNotices();
     }, [moimId]);
+
+    const handleSearch = () => {
+        setPage(1);
+        setNotices([]);
+        fetchNotices();
+        console.log("검색", notices);
+    };
 
     return (
         <StyledBasicContainer>
@@ -49,12 +89,14 @@ const MoimNoticeList = ({ isMainPage = stubFalse, setActiveTab, setBoardType, se
                                         </StyledText>
                                     </StyledMainHeaderCell>
                                     <StyledMainHeaderCell>
-                                        <StyledText
-                                            style={{ fontSize: "1rem" }}
-                                            onClick={() => navi(`/${moimId}/create-board`, { state: { boardType: "NOTICE" } })}
-                                        >
-                                            글쓰기
-                                        </StyledText>
+                                        {userRole.isLeader ? (
+                                            <StyledText
+                                                style={{ fontSize: "1rem" }}
+                                                onClick={() => navi(`/${moimId}/create-board`, { state: { boardType: "NOTICE" } })}
+                                            >
+                                                글쓰기
+                                            </StyledText>
+                                        ) : null}
                                     </StyledMainHeaderCell>
                                 </StyledRow>
                             ) : (
@@ -84,6 +126,55 @@ const MoimNoticeList = ({ isMainPage = stubFalse, setActiveTab, setBoardType, se
                             ))}
                         </tbody>
                     </Styled>
+                    {!isMainPage ? (
+                        <StyledFooter>
+                            <ListPagination></ListPagination>
+                            <ListMoimSearchContainer>
+                                {userRole.isLeader ? (
+                                    <ListMoimTextField
+                                        style={{ marginLeft: "5rem" }}
+                                        size="small"
+                                        variant="outlined"
+                                        placeholder="검색어를 입력하세요."
+                                        onChange={(e) => setKeyword(e.target.value)} />
+                                ) : (
+                                    <ListMoimTextField
+                                        style={{ marginLeft: "10rem" }}
+                                        size="small"
+                                        variant="outlined"
+                                        placeholder="검색어를 입력하세요."
+                                        onChange={(e) => setKeyword(e.target.value)} />
+                                )}
+                                <ListMoimSelect
+                                    value={searchType}
+                                    displayEmpty
+                                    size="small"
+                                    onChange={(e) => setSearchType(e.target.value)}>
+                                    <ListMoimMenuItem value="all">전체</ListMoimMenuItem>
+                                    <ListMoimMenuItem value="title">제목</ListMoimMenuItem>
+                                    <ListMoimMenuItem value="content">내용</ListMoimMenuItem>
+                                    <ListMoimMenuItem value="nickname">작성자</ListMoimMenuItem>
+                                </ListMoimSelect>
+                                <ListMoimSearchButton variant="contained" size="normal" onClick={handleSearch}>검색</ListMoimSearchButton>
+                                {userRole.isLeader ? (
+                                    <ListMoimSearchButton
+                                        variant="contained"
+                                        size="normal"
+                                        onClick={() => navi(`/${moimId}/create-board`, { state: { boardType: "NOTICE" } })}>
+                                        글쓰기
+                                    </ListMoimSearchButton>
+                                ) : (
+                                    <ListMoimSearchButton
+                                        variant="contained"
+                                        size="normal"
+                                        style={{ display: "none" }}
+                                        onClick={() => navi(`/${moimId}/create-board`, { state: { boardType: "NOTICE" } })}>
+                                        글쓰기
+                                    </ListMoimSearchButton>
+                                )}
+                            </ListMoimSearchContainer>
+                        </StyledFooter>
+                    ) : null}
                 </StyledContainer>
             </StyledPaper>
         </StyledBasicContainer>
